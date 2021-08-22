12ab85db8a0098a08e27be1fd0a66786c76b165a
Milton <A01634505@itesm.mx>
2021-08-22 02:37:26 -0500

    Phonebook project succesfully running on the folder structure boiler plate

---
+++ server/index.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
+
+const Person = require('./models/Person');
+
+app.use(express.static('build'));
+app.use(cors());
+
+app.use(express.json());
+morgan.token('body', function (req) {
+    return req.body.name && req.body.number
+        ? `{name: ${req.body.name}, number:${req.body.number}}`
+        : '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+// let persons = [
+//     { id: 1, name: 'Arto Hellas', number: '040-123456' },
+//     { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
+//     { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
+//     { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' },
+// ];
+
+app.get('/persons', (req, res) => {
+    Person.find({}).then((persons) => {
+        res.json(persons);
+    });
+});
+
+app.get('/info', (req, res) => {
+    Person.find({}).then((persons) => {
+        const currDate = new Date();
+        res.send(`<div>
+        <h3>Phonebook has info for ${persons.length} people</h3>
+        <p>${currDate}</p>
+    </div>`);
+    });
+});
+
+app.get('/persons/:id', (req, res, next) => {
+    const id = req.params.id;
+    // const person = persons.find((person) => person.id === parseInt(id));
+    Person.findById(id)
+        .then((person) => {
+            person ? res.json(person) : res.status(404).end();
+        })
+        .catch((err) => {
+            next(err);
+        });
+});
+
+app.post('/persons', (req, res, next) => {
+    const body = req.body;
+    // if (!body.name || !body.number) {
+    //     return res.status(400).json({
+    //         error: 'Name or number missing',
+    //     });
+    // }
+    // // else if (persons.some((person) => person.name === body.name)) {
+    // //     return res.status(400).json({
+    // //         error: 'Name must be unique',
+    // //     });
+    // // }
+
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number,
+    });
+
+    newPerson
+        .save()
+        .then((savedPerson) => {
+            res.json(savedPerson);
+        })
+        .catch((err) => next(err));
+});
+
+app.put('/persons/:id', (req, res, next) => {
+    const id = req.params.id;
+    const body = req.body;
+
+    const person = { name: body.name, number: body.number };
+    Person.findByIdAndUpdate(id, person, { new: true, runValidators: true, context: 'query' })
+        .then((updatedPerson) => {
+            res.json(updatedPerson);
+        })
+        .catch((error) => next(error));
+});
+
+app.delete('/persons/:id', (req, res, next) => {
+    const id = req.params.id;
+
+    Person.findByIdAndRemove(id)
+        .then(() => {
+            res.status(204).end();
+        })
+        .catch((err) => next(err));
+});
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message });
+    }
+
+    next(error);
+};
+
+app.use(errorHandler);
+
+module.exports = app;
+
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//     console.log(`Server running at port ${PORT}`);
+// });
---
+++ server/models/Person.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+require('dotenv').config();
+
+// const name = process.argv[2] || null;
+// const number = process.argv[3] || null;
+
+const url = process.env.MONGODB_URI;
+
+console.log('Connecting to', url);
+
+mongoose
+    .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Could not connect to MongoDB', err.message);
+    });
+
+const personSchema = new mongoose.Schema({
+    name: { type: String, minlength: 3, required: true, unique: true },
+    number: { type: String, minlength: 8, required: true },
+});
+
+personSchema.plugin(uniqueValidator);
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    },
+});
+
+// const Person = mongoose.model('Person', personSchema);
+
+// if (name && number) {
+//     const person = new Person({
+//         name,
+//         number,
+//     });
+
+//     person.save().then((res) => {
+//         console.log(`${name} number: ${number} added!`);
+//         mongoose.connection.close();
+//     });
+// } else {
+//     Person.find({}).then((res) => {
+//         console.log('Phonebook');
+//         res.forEach((person) => {
+//             console.log(`${person.name} ${person.number}`);
+//         });
+//         mongoose.connection.close();
+//     });
+// }
+
+module.exports = mongoose.model('Person', personSchema);
