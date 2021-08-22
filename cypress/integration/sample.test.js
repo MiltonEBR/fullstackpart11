08397d49078017d067963b2a8e6f1423f7987f24
Milton <A01634505@itesm.mx>
2021-08-22 13:40:53 -0500

    Fixed cypress port

--- cypress/integration/sample.test.js
+++ cypress/integration/sample.test.js
@@ -1,7 +1,7 @@
 
 describe('Phonebook', function() {
     it('front page can be opened', function() {
-      cy.visit('http://localhost:3001')
+      cy.visit('http://localhost:8000')
       cy.contains('Phonebook')
       cy.contains('Add new person')
     })
